af4fa9430aa4412970cb814723b850f274e687bb
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-03-07 21:23:47 -0300

    add RTL config and first test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$'
+  ],
+  moduleDirectories: ['node_modules', '.'],
+  // setupFilesAfterEnv: ['jest-extended'],
+  resetMocks: true,
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>Boilerplate viteess</h1>
+    </main>
+  )
+}
+
+export default Main
